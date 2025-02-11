3e0229fe73f37d14ba475209a94944aa01e0b72a
Jefik37 <58911866+Jefik37@users.noreply.github.com>
2025-02-11 10:12:13 -0300

    Update baseconvertion.lua
    Optimized code.

--- baseconvertion.lua
+++ baseconvertion.lua
@@ -1,163 +1,91 @@
 local F = {}
 
-local function reversetable(tbl)
-
-    local n = #tbl
-    for i = 1, math.floor(n / 2) do
-        tbl[i], tbl[n - i + 1] = tbl[n - i + 1], tbl[i]
-    end
-
-    return tbl
-
-end
-
-local function totable(str)
-
-    if str == nil then return nil end
-
-    local tbl = {}
-    for i = 1, #str do
-        tbl[i] = str:sub(i, i)
-    end
-    return tbl
-end
-
-local function printtable(tbl)
-    tbl = table.concat(tbl)
-    print(tbl)
-end
-
-local function split(str, sep)
-    local result = {}
-    local pattern = string.format("([^%s]+)", sep)
-    for part in string.gmatch(str, pattern) do
-        table.insert(result, part)
-    end
-    return result
-end
-
-local function contains(tbl, value)
-    for index, v in ipairs(tbl) do
-        if v == value then
-            return index
-        end
-    end
-    return false
-end
-
 function F.dectobase(number, base)
-    if base < 2 or base ~= math.floor(base)then return nil end
-    number = tonumber(number)
+    number = tostring(number)
+    base = tonumber(base) or 12
+    if base < 2 or base ~= math.floor(base) then return nil end
+    local sign, integer, fractional = number:match('^([+-]?)(%d+)(%.?%d*)')
 
+    ------------------ INTEGER PART ------------------
     local integers = {}
+    integer = tonumber(integer)
+
+    while integer ~= 0 do
+        local remainder = integer % base
+        if remainder>=10 then remainder = string.char(55+remainder) end
+        integer = math.floor(integer/base)
+        table.insert(integers, 1, remainder) 
+    end
+    integers = table.concat(integers)
+    ------------------ INTEGER PART ------------------
+
+    ------------------ FRACTIONAL PART ------------------
     local fractionals = {}
-    local dividend = 0
-    local remainder = 0
-    local negative = false
-
-    if (string.sub(number,1,1)) == '-' then
-        negative = true
-        number = tonumber(string.sub(number,2,-1))
+    fractional = tonumber(fractional) or 0
+    while fractional ~= 0 do
+        fractional = fractional * base
+        integer = math.floor(fractional)
+        fractional = fractional - integer
+        if integer>=10 then integer = string.char(55+integer) end
+        table.insert(fractionals, integer)
     end
-
-    local integer = math.floor(number)
-    local fractional = number-integer
-
-    ------------------ INTEGER PART ------------------
-    while integer >= base do
-        remainder = integer % base
-        table.insert(integers, remainder)
-        integer = math.floor(integer / base)
-    end
-
-    table.insert(integers,integer)
-
-    for i = 1, #integers do 
-        if integers[i]>=10 then
-            integers[i] = string.char(55+integers[i])
-        end
-    end
-
-    integers = reversetable(integers)
-
-    if negative then
-        table.insert(integers,1,'-')
-    end
-
-    integers = table.concat(integers,'')
-    ------------------ INTEGER PART ------------------
-
-    ------------------ FRACTIONAL PART ------------------
-    if fractional ~=0 then
-        while fractional ~= 0 do
-            fractional = fractional * base
-            integer = math.floor(fractional)
-            fractional = fractional - integer
-            table.insert(fractionals, integer)
-        end
-        
-        for i = 1, #fractionals do 
-            if fractionals[i]>=10 then
-                fractionals[i] = string.char(55+fractionals[i])
-            end
-        end
-
-        fractionals = table.concat(fractionals,'')
-
-        integers = table.concat({integers,fractionals}, '.')
-    end
+    if integers == '' then integers = '0' end
+    fractionals = table.concat(fractionals):gsub('([%d%u]+)','.%1')
     ------------------ FRACTIONAL PART ------------------
 
-    return integers
+    ------------------ REPETITION FIX ------------------
+    local precision = 11
+    if fractionals:sub(precision+1,precision+1) ~= '' then
+        local add_sub = 1
+        if F.basetodec(fractionals:sub(precision+1,precision+1), base) < base/2 then
+            add_sub = 0
+        end
+        local x = F.dectobase(F.basetodec(fractionals:sub(2,precision),base)+add_sub, base)
+        while #x<precision-1 do x = '0'..x end
+        if #x>precision-1 then
+            integers = F.basetodec(integers,base)
+            integers = F.basetodec(integers+1, base)
+            x = x:sub(2)
+        end
+        -- print(x)
+        fractionals = '.'..x
+    end
+    ------------------ REPETITION FIX ------------------
 
+    return sign..integers..fractionals:gsub('0+$', '')
 end
 
 function F.basetodec(number, base)
-    if base < 2 or base ~= math.floor(base)then return nil end
+    base = tonumber(base) or 12
+    number = tostring(number)
+    if base < 2 or base ~= math.floor(base) then return nil end
 
-    local negative = false
-    if (string.sub(number,1,1)) == '-' then
-        negative = true
-        number = tostring(string.sub(number,2,-1))
-    end
-
-    number = split(number,'.')
-    local integer = totable(string.reverse(number[1]))
-    local fractional = totable(number[2])
+    local sign, integer, fractional = number:match('^([+-]?)([%d%u]+)(%.?[%d%u]*)')
     local result = 0
 
     ------------------ INTEGER PART ------------------
-    for i =1, #integer do
+    local result = 0
 
-        if string.byte(integer[i])>=65 then
-            integer[i] = string.byte(integer[i])-55
+    for i = 1, #integer do
+        local char_ = integer:sub(i,i)
+        if string.byte(char_)>=65 then
+            char_ = string.byte(char_)-55
         end
-
-        result = result + integer[i]*base^(i-1)
-        
+        result = result + char_*base^(#integer-i)
     end
     ------------------ INTEGER PART ------------------
 
     ------------------ FRACTIONAL PART ------------------
-    if fractional ~=nil then
-        for i =1, #fractional do
-
-            if string.byte(fractional[i])>=65 then
-                fractional[i] = string.byte(fractional[i])-55
-            end
-    
-            result = result + fractional[i]*base^(-i)
-            
+    for i = 1, #fractional-1 do
+        local char_ = fractional:sub(i+1,i+1)
+        if string.byte(char_)>=65 then
+            char_ = string.byte(char_)-55
         end
+        result = result + char_*base^(-i)
     end
     ------------------ FRACTIONAL PART ------------------
 
-    if negative then
-        result = -result
-    end
-
-    return result
-
+    return tonumber(string.format('%.40f',tonumber(sign..result)))
 end
 
 function F.basetobase(number, base_origin, base_goal)
